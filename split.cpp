3eb30f537f96d964c0cc4ec47e2fc870156f75e2
Megan Lai <mclai@usc.edu>
2025-02-01 01:24:00 +0000

    adding comment to part 1

--- split.cpp
+++ split.cpp
@@ -16,17 +16,21 @@ the function below should be the only one in this file.
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
+  // if we reached the end of 'in', just return
   if (in==nullptr){
     return;
   }
 
+  // if the value in 'in' is even, move it to the evens list
   else if (in->value % 2 == 0){
+    // if this is the first value in the evens list, set the list to 'in' and recursively call the function again
     if (evens==nullptr){
       evens = in;
       in = in->next;
       evens->next = nullptr;
       split(in, odds, evens);
     }
+    // if there are already nodes in the list, point it to 'in', and recursively call the function again
     else{
       evens->next = in;
       in = in->next;
@@ -35,13 +39,16 @@ void split(Node*& in, Node*& odds, Node*& evens)
     }
   }
 
+  // if the value in 'in' is odd, move it to the odds list
   else{
+    // if this is the first value in the odds list, set the list to 'in' and recursively call the function again
     if (odds==nullptr){
       odds = in;
       in = in->next;
       odds->next = nullptr;
       split(in, odds, evens);
     }
+    // if there are already nodes in the list, point it to 'in', and recursively call the function again
     else{
       odds->next = in;
       in = in->next;
