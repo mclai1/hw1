e4134af4feaaabc05615925e868ca084da9b498c
Megan Lai <mclai@usc.edu>
2025-02-01 01:15:20 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,33 @@
 
 int main(int argc, char* argv[])
 {
+  // create a new ULListStr object and add strings
+  ULListStr temp;
+  temp.push_front("one");
+  temp.push_back("two");
+  temp.push_back("three");
+  temp.push_back("four");
+  temp.push_back("five");
+  temp.push_back("six");
+  temp.push_back("seven");
+  temp.push_back("eight");
+  temp.push_back("nine");
+  temp.push_back("ten");
+  temp.push_back("eleven");
+  temp.push_back("twelve");
+  temp.push_back("thirteen");
+  temp.push_front("zero");
 
+  // test if the strings were added correctly with get, front(), and back()
+  std::cout << temp.get(0) << std::endl;
+  std::cout << temp.get(1) << std::endl;
+  std::cout << temp.front() << std::endl;
+  std::cout << temp.back() << std::endl;
+
+  // try pop_front()
+  temp.pop_front();
+  temp.pop_front();
+  std::cout << temp.get(0) << std::endl;
+
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,134 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val){
+  // If the list is empty, add a new Item and initialize the value, first, and last. Point the tail_ to the head_.
+  if (empty()){
+    head_ = new Item;
+    head_->next = nullptr;
+    head_->val[head_->first] = val;
+    head_->last++;
+    tail_ = head_;
+  }
+  // If there is still room in the array, add the value to the end
+  else if (tail_ -> last < ARRSIZE){
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+  }
+  // If there is no more room in the array, add a new node to the end and store the value in the array.
+  else{
+    Item *temp = new Item;
+    temp->val[temp->first] = val;
+    temp->last++;
+    temp->next = nullptr;
+    temp->prev = tail_;
+    tail_->next = temp;
+    tail_ = temp;
+  }
+  // Increment the size of the extended array
+  size_++;
+}
+
+void ULListStr::pop_back(){
+  // only pop back if there is something in the array/list
+  if (!empty()){
+    // move the index of the last position and decrement the size
+    tail_->last--;
+    size_--;
+    // if there are no more values in the last array, delete the entire node
+    if (tail_ -> last - tail_ -> first <= 0){
+      Item *temp = tail_;
+      tail_ = tail_->prev;
+      if (tail_ != nullptr){
+        tail_->next = nullptr;
+      }
+      else{
+        head_ = nullptr;
+      }
+      delete temp;
+    }
+  }
+}
+
+void ULListStr::push_front(const std::string& val){
+  // If the list is empty, add a new Item and initialize the value, first, and last. Point the tail_ to the head_.
+  if(empty()){
+    head_ = new Item;
+    head_->next = nullptr;
+    head_->val[head_->first] = val;
+    head_->last++;
+    tail_ = head_;
+  }
+  // If there is still room in the array, add the value to the front
+  else if (head_ -> first > 0){
+    head_->val[head_->first - 1] = val;
+    head_->first--;
+  }
+  // If there is no more room in the array, add a new node to the front and store the value in the array.
+  else {
+    Item *temp = new Item;
+    temp->val[ARRSIZE-1] = val;
+    temp->last = ARRSIZE;
+    temp->first= ARRSIZE-1;
+    temp->next = head_;
+    temp->prev = nullptr;
+    head_->prev = temp;
+    head_ = head_->prev;
+  }
+  // Increment the size of the extended array
+  size_++;
+}
+
+void ULListStr::pop_front(){
+  // only pop front if there is something in the array/list
+  if (!empty()){
+    // move the index of the first position and decrement the size
+    head_->first++;
+    size_--;
+    // if there are no more values in the first array, delete the entire node
+    if (head_ -> last - head_ -> first <= 0){
+      Item *temp = head_;
+      head_ = head_->next;
+      if (head_ != nullptr){
+        head_->prev = nullptr;
+      }
+      else{
+        tail_ = nullptr;
+      }
+      delete temp;
+    }
+  }
+}
+
+std::string const & ULListStr::back() const{
+  // return the last element of the array in the tail
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const{
+  // return the first element of the array in the head
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  // check if loc is valid
+  if (loc>=0 && loc<size()){
+    // store the head in a temp variable to loop through the nodes
+    Item *temp = head_;
+    while (temp!=NULL){
+      // check if the loc can be found in the node, return the loc if it is found in the node 
+      size_t arraySize = temp->last - temp->first;
+      if(arraySize > loc){
+        return &(temp->val[temp->first + loc]);
+      }
+      // if not, move to the next node and adjust the loc
+      temp = temp->next;
+      loc -= arraySize;
+    }
+  }
+  // loc is not valid or not found, returns null
+  return NULL;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
